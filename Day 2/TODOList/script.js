94d80b46625aab2974d41c30443e014b721d8125
Niksha_Patel <niksha.patel@bacancy.com>
2024-03-19 22:29:04 +0530

    changed TODO

--- Day 2/TODOList/script.js
+++ Day 2/TODOList/script.js
@@ -2,7 +2,8 @@
 function addToList() {
     const inputBox = document.getElementById('input-box');
     const inputValue = inputBox.value; 
-
+ 
+    debugger;
     if (inputValue) { 
         const hiddenContainer = document.createElement('div');
         hiddenContainer.classList.add('hidden-container');
@@ -14,7 +15,21 @@ function addToList() {
         paragraph.id = 'add-items-here';
         paragraph.textContent = inputValue;
 
+        const closeButton = document.createElement('button');
+        closeButton.classList.add('close-button');
+        closeButton.textContent = 'X';
+
         newContainer.appendChild(paragraph);
+        newContainer.appendChild(closeButton);
+
+        // const strike = document.createElement("strike");
+        // strike.textContent=inputBox;
+        closeButton.addEventListener('click', function() {
+                // paragraph.textContent=strike;
+                newContainer.remove();
+
+        });
+
         hiddenContainer.appendChild(newContainer);
 
         const listContainer = document.querySelector('.list-container');
--- Day1 Calculator Design/script.js
+++ Day1 Calculator Design/script.js
@@ -1,19 +1,20 @@
 function ClearAll() {
-    var input = document.getElementById('inputBox');
-    input.value = '';
+   var input = document.getElementById('inputBox');
+   input.value = '';
  }
  function Back() {
-    var input = document.getElementById('inputBox');
-    input.value = ev.value.slice(0,-1);
+   var input = document.getElementById('inputBox');
+   input.value = input.value.slice(0,-1);
  }
 
 function Input(val) {
-    var v = document.getElementById('inputBox');
-    v.value += val;
-    console.log(v.value)
+   var v = document.getElementById('inputBox');
+   v.value += val;
+   console.log(v.value)
  }
  function Result() {
-    var num1 = document.getElementById('inputBox').value;
-    var num2 = eval(num1);
-    document.getElementById('res').value = num2;
+   var num1 = document.getElementById('inputBox').value;
+   var num2 = eval(num1);
+   console.log(num2);
+   document.getElementById('inputBox').value = num2;
  }
